7519a20a36e2c8daa837213fa57b3d633c170d97
DesHeaden <headendes@gmail.com>
2025-05-03 13:53:37 -0400

    Update cti_110_node.js

--- cti_110_node.js
+++ cti_110_node.js
@@ -30,9 +30,11 @@ app.use("/", router);
 
 router.get('/api/grades',function(req, res){
     pool.query(
-        `SELECT s.first_name, s.last_name, a.grade
-        FROM students s
-        JOIN assignments a ON s.student_id = a.student_id;
+        `SELECT Students.student_id, first_name, last_name, AVG(assignments.grade) as Total_grade \
+            FROM Students \
+            LEFT JOIN Assignments ON Assignments.student_id = Students.student_id \
+            GROUP BY Students.student_id \
+            ORDER BY total_grade DESC`,
         [],
         function( err, result){
             if(err)
